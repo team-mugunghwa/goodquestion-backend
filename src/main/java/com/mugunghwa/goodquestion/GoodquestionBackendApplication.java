b159e96f48d783cc302de22157ddccf8128114b5
leeseowoo <leeseowoo.kr@gmail.com>
2026-08-07 23:06:08 +0900

    Initial commit

---
+++ src/main/java/com/mugunghwa/goodquestion/GoodquestionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mugunghwa.goodquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodquestionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodquestionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mugunghwa/goodquestion/GoodquestionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugunghwa.goodquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodquestionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
